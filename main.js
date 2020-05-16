66abaaa0cee06f61a8ba19ec40db75a131c4eeaa
Nattapon Lueakaew <nsoooooooooosn@gmail.com>
2020-05-17 03:45:29 +0700

    :fire: delete addUser in main

--- main.js
+++ main.js
@@ -2,18 +2,6 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts"
 import { APP_HOST, APP_PORT } from "./config.js"
 import routerUser from "./users/users.controller.js"
 
-export const addUser = async ({ request, response }) => {
-  const body = await request.body();
-  const { name } = body.value;
-  let result = await client.execute(`INSERT INTO users(name) values(?)`, [
-    name,
-  ]);
-  if (result) {
-    response.status = 200;
-    response.body = { message: "add success" };
-  }
-};
-
 const app = new Application();
 
 app.use(routerUser.routes());
